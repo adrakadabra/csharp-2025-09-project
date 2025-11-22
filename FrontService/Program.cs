ac6cddd66e8af72ddad56ec2f9ebdf2deb684177
Arkhi Arkhi <ilya.arhipov@fasttech.dev>
2025-11-22 21:15:54 +0500

    Добавление/редактирование страниц для Админки

--- FrontService/Program.cs
+++ FrontService/Program.cs
@@ -1,8 +1,11 @@
 using FrontService.Services.Auth;
 using FrontService.Services.Auth.FrontService.Services.Auth;
 using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices();
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
