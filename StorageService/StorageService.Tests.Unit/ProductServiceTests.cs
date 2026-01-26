79b753e3255f598cd2a2da345b707f17e9a524f0
Михаил Пожалостин <m.pozhalostin@whyhappen.ru>
2026-01-26 10:59:04 +0300

    fix tests

--- StorageService/StorageService.Tests.Unit/ProductServiceTests.cs
+++ StorageService/StorageService.Tests.Unit/ProductServiceTests.cs
@@ -4,6 +4,7 @@ using StorageService.Api.Application.Services;
 using StorageService.Api.Application.DTOs;
 using StorageService.Api.Domain.Entities;
 using StorageService.Api.Infrastructure.Interfaces;
+using StorageService.Api.Application.Interfaces;
 
 namespace StorageService.Tests.Unit;
 
@@ -13,11 +14,16 @@ public class ProductServiceTests
     public async Task CreateAsync_Should_Call_Repository_And_Return_Dto()
     {
         var repoMock = new Mock<IProductRepository>();
+        var catServiceMock = new Mock<ICategoryService>();
+        var manuServiceMock = new Mock<IManufacturerService>();
+        var sectionServiceMock = new Mock<ISectionService>();
         repoMock.Setup(r => r.AddAsync(It.IsAny<Product>()))
             .ReturnsAsync((Product p) => p);
 
-        var service = new ProductService(repoMock.Object);
-        var dto = new CreateProductDto { Name = "Test", Description = "Desc", Quantity = 5};
+        var service = new ProductService(repoMock.Object, catServiceMock.Object,
+            manuServiceMock.Object, sectionServiceMock.Object);
+
+        var dto = new CreateProductDto("Test", "Desc", 5, 100, "test", "test", "M3");
 
         var result = await service.CreateAsync(dto);
 
@@ -30,9 +36,13 @@ public class ProductServiceTests
     public async Task UpdateAsync_Returns_False_When_NotFound()
     {
         var repoMock = new Mock<IProductRepository>();
+        var catServiceMock = new Mock<ICategoryService>();
+        var manuServiceMock = new Mock<IManufacturerService>();
+        var sectionServiceMock = new Mock<ISectionService>();
         repoMock.Setup(r => r.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Product?)null);
 
-        var service = new ProductService(repoMock.Object);
+        var service = new ProductService(repoMock.Object, catServiceMock.Object,
+            manuServiceMock.Object, sectionServiceMock.Object);
         var ok = await service.UpdateAsync(Guid.NewGuid(), new UpdateProductDto { Name = "X", Quantity = 1, Price = 1m });
 
         ok.Should().BeFalse();
