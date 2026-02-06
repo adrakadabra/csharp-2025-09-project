803ed6bb876e131f2dc23da99047f9c552918923
Purrrrgrammer <miss.levinaa@yandex.ru>
2026-02-06 16:17:07 +0700

    add Picker controller : GetPickers, GetPickerById, CreatePiker, UpdatePicker

--- OrderPickingService/OrderPickingService.Infrastructure.Database/Abstractions/IDataBaseContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using OrderPickingService.Infrastructure.Database.Dtos;
-
-namespace OrderPickingService.Infrastructure.Database.Abstractions;
-
-internal interface IDataBaseContext
-{ 
-    DbSet<Picker> Pickers { get; }
-}
--- OrderPickingService/OrderPickingService.Infrastructure.Database/PickerRepository.cs
+++ OrderPickingService/OrderPickingService.Infrastructure.Database/PickerRepository.cs
@@ -1,17 +1,56 @@
-
 using Microsoft.EntityFrameworkCore;
 using OrderPickingService.Domain.Entities;
-using OrderPickingService.Infrastructure.Database.Abstractions;
+using OrderPickingService.Infrastructure.Database.Entities;
 using OrderPickingService.Services.Repositories.Abstractions;
 
 namespace OrderPickingService.Infrastructure.Database;
 
-internal sealed class PickerRepository(IDataBaseContext databaseContext) : IPickerRepository
+internal sealed class PickerRepository(DatabaseContext databaseContext) : IPickerRepository
 {
-    public Task<List<Picker>> GetPickers()
+    public async Task<List<Picker>> GetAllAsync()
     {
-        return databaseContext.Pickers
-            .Select( x => Picker.Create(x.Id, x.FirstName, x.LastName))
+        return await databaseContext.Pickers
+            .Select( x => Picker.Load(x.Id, x.FirstName, x.LastName))
             .ToListAsync();
+    }
+
+    public async Task<Picker?> GetByIdAsync(long id)
+    {
+        var picker = await databaseContext.Pickers
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+        
+        return picker == null ? null : Picker.Load(picker.Id, picker.FirstName, picker.LastName);
+    }
+
+    public async Task<Picker> CreateAsync(Picker picker)
+    {
+        var pickerEntity = PickerEntity.Create(
+            picker.FirstName,
+            picker.LastName);
+        
+        await databaseContext.Pickers.AddAsync(pickerEntity);
+        await databaseContext.SaveChangesAsync();
+        
+        return Picker.Load(pickerEntity.Id, pickerEntity.FirstName, pickerEntity.LastName);
+    }
+
+    public async Task UpdateAsync(Picker picker)
+    {
+        var entity = await databaseContext.Pickers.FindAsync(picker.Id);
+        
+        if(entity == null)
+            throw new KeyNotFoundException($"Picker with id {picker.Id} not found in database");
+        
+        if(entity.FirstName == picker.FirstName && 
+           entity.LastName == picker.LastName)
+            return;
+        
+        entity.FirstName = picker.FirstName;
+        entity.LastName = picker.LastName;
+        entity.UpdatedAt = DateTime.UtcNow;
+        entity.UpdatedBy = "system";
+        
+        await databaseContext.SaveChangesAsync();
     }
 }
